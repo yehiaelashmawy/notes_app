4ca9b97030358f39f9ed94fd069ef4176f7324d1
ahmed-alashmawy <ahmedashmaw33@gmail.com>
2025-02-07 15:37:13 +0200

    create edit notes view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,57 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_notes.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: const Color(0xffFFCC80),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return const EditNotesView();
+          },
+        ));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: const Color(0xffFFCC80),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter tips',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
               ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 8),
+                child: Text(
+                  'You should fenisih every things every day ',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.4),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 26,
+                  )),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16.0, bottom: 8),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'You should fenisih every things every day ',
+                'May21,2025',
                 style: TextStyle(
                   color: Colors.black.withOpacity(0.4),
-                  fontSize: 18,
+                  fontSize: 12,
                 ),
               ),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 26,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May21,2025',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.4),
-                fontSize: 12,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
