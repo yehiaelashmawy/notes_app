062968eb14ba39962ff749123382da4926b08cd4
ahmed-alashmawy <ahmedashmaw33@gmail.com>
2025-02-07 16:44:42 +0200

    create notes modrl

---
+++ lib/models/notes_model.dart
@@ -0,0 +1,11 @@
+class NotesModel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+  NotesModel(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
+}
