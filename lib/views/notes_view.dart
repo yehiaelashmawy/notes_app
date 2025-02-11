2b42c7622d8cf4bbe675c018f591eb9d940e94d6
ahmed-alashmawy <ahmedashmaw33@gmail.com>
2025-02-11 22:32:42 +0200

    fix bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -15,6 +15,7 @@ class NotesView extends StatelessWidget {
           backgroundColor: kPrimaryColor,
           onPressed: () {
             showModalBottomSheet(
+                isScrollControlled: true,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(16),
                 ),
--- lib/views/widgets/add_note.dart
+++ lib/views/widgets/add_note.dart
@@ -21,9 +21,13 @@ class AddNote extends StatelessWidget {
         builder: (context, state) {
           return AbsorbPointer(
             absorbing: state is AddNoteLoading ? true : false,
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: 16.0,
+                right: 16,
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              child: const SingleChildScrollView(
                 child: AddNotesForme(),
               ),
             ),
