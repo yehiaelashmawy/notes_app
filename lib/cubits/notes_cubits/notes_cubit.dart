a76b9eb93cbbaa6261473fc08c4a14bbd8aa9bac
ahmed-alashmawy <ahmedashmaw33@gmail.com>
2025-02-11 22:52:47 +0200

    create fetch note method

--- lib/cubits/notes_cubits/notes_cubit.dart
+++ lib/cubits/notes_cubits/notes_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constans.dart';
 import 'package:notes_app/models/notes_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  featchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NotesModel>(kNotesBox);
+      List<NotesModel> note = notesBox.values.toList();
+      emit(NotesScusse(note));
+    } catch (e) {
+      emit(NotesFailur(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constans.dart';
+import 'package:notes_app/cubits/notes_cubits/notes_cubit.dart';
 import 'package:notes_app/views/widgets/add_note.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
@@ -8,26 +10,29 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(right: 8.0),
-        child: FloatingActionButton(
-          backgroundColor: kPrimaryColor,
-          onPressed: () {
-            showModalBottomSheet(
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                context: context,
-                builder: (context) {
-                  return const AddNote();
-                });
-          },
-          child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: FloatingActionButton(
+            backgroundColor: kPrimaryColor,
+            onPressed: () {
+              showModalBottomSheet(
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return const AddNote();
+                  });
+            },
+            child: const Icon(Icons.add),
+          ),
         ),
+        body: const NotesViweBody(),
       ),
-      body: const NotesViweBody(),
     );
   }
 }
