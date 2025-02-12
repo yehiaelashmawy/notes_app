3f39ab31f20f1ceaca8b05f01afdcd5e9da7f5f4
ahmed-alashmawy <ahmedashmaw33@gmail.com>
2025-02-12 23:52:53 +0200

    Add note buttom sheet done

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,7 +1,7 @@
 // ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:meta/meta.dart';
 import 'package:notes_app/constans.dart';
 import 'package:notes_app/models/notes_model.dart';
 
@@ -9,8 +9,10 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  Color color = Color(0xff695958);
 
   addnote(NotesModel note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NotesModel>(kNotesBox);
